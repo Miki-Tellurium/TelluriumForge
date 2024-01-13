d6b4804e1c85e0ab7181b7e652817132751433bf
Miki_Tellurium <michelegamer14@gmail.com>
2024-01-13 16:05:05 +0100

    Added static getter for mod id

--- src/main/java/com/mikitellurium/telluriumforge/TelluriumForge.java
+++ src/main/java/com/mikitellurium/telluriumforge/TelluriumForge.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 public class TelluriumForge implements ModInitializer {
 
     private static final Logger LOGGER = LoggerFactory.getLogger("telluriumforge");
+	private static final String MOD_ID = "telluriumforge";
 
 	@Override
 	public void onInitialize() {
@@ -16,6 +17,10 @@ public class TelluriumForge implements ModInitializer {
 
 	public static Logger logger() {
 		return LOGGER;
+	}
+
+	public static String modId() {
+		return MOD_ID;
 	}
 
 }
