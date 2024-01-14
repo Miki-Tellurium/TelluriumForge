e8965b0b4b17ebf7bf86cf7fcaf3e7e3d6e2893d
Miki_Tellurium <michelegamer14@gmail.com>
2024-01-14 11:22:50 +0100

    Minor changes

--- src/main/java/com/mikitellurium/telluriumforge/TelluriumForge.java
+++ src/main/java/com/mikitellurium/telluriumforge/TelluriumForge.java
@@ -1,7 +1,13 @@
 package com.mikitellurium.telluriumforge;
 
+import com.mikitellurium.telluriumforge.test.ExampleConfig;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
+import net.fabricmc.fabric.api.event.EventFactory;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import net.minecraft.world.WorldEvents;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,7 +18,8 @@ public class TelluriumForge implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		ServerEntityEvents.ENTITY_LOAD.register(ExampleConfig::onWorldPlayerJoin);
+		ExampleConfig.buildConfig();
 	}
 
 	public static Logger logger() {
