f63d815c7066dd41d9b3f1084aaa96d046b67fcc
Miki_Tellurium <michelegamer14@gmail.com>
2024-01-15 17:29:27 +0100

    Removed useless credits and license stuff

--- src/main/java/com/mikitellurium/telluriumforge/config/TelluriumConfig.java
+++ src/main/java/com/mikitellurium/telluriumforge/config/TelluriumConfig.java
@@ -9,37 +9,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-/*
- * You can use and modify this freely if you want, just respect the License
- * and credit me.
- *
- * MIT License
- *
- * Copyright (c) 2023 Miki_Tellurium
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-
 /**
  * A class used to create and load simple config files.
- * @author Miki Tellurium
- * @version 1.5.0-Fabric
  */
 @SuppressWarnings("rawtypes")
 public class TelluriumConfig {
