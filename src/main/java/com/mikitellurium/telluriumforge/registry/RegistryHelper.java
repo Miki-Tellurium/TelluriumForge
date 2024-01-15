bc5036ffb23bc9d9a095e52331548130c88069ff
Miki_Tellurium <michelegamer14@gmail.com>
2024-01-15 19:10:23 +0100

    Added java docs

--- src/main/java/com/mikitellurium/telluriumforge/registry/RegistryHelper.java
+++ src/main/java/com/mikitellurium/telluriumforge/registry/RegistryHelper.java
@@ -6,6 +6,9 @@ import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
 import net.minecraft.client.gui.screen.ingame.HandledScreens;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
@@ -17,53 +20,155 @@ import net.minecraft.screen.ScreenHandler;
 import net.minecraft.screen.ScreenHandlerType;
 import net.minecraft.util.Identifier;
 
+/**
+ * The {@code RegistryHelper} class provides utility methods for registering various game elements
+ * such as blocks, items, block entities, etc.
+ */
 public class RegistryHelper {
 
+    /**
+     * The mod id associated with this {@code RegistryHelper}.
+     */
     private final String modId;
 
+    /**
+     * Constructs a new {@code RegistryHelper} with the specified mod id.
+     * The mod id will be used as the {@code Identifier} namespace.
+     *
+     * @param modId The mod identifier
+     */
     public RegistryHelper(String modId) {
         this.modId = modId;
     }
 
-    public Block registerBlock(String name, Block block) {
-        return Registry.register(Registries.BLOCK, getIdentifier(name), block);
+    /**
+     * Registers a block with the game registry.
+     *
+     * @param id  The id of the block
+     * @param block The block to register
+     * @return The registered block
+     */
+    public Block registerBlock(String id, Block block) {
+        return Registry.register(Registries.BLOCK, getIdentifier(id), block);
     }
 
-    public Block registerBlockWithItem(String name, Block block) {
-        Block blockToReturn = registerBlock(name, block);
-        registerItem(name, new BlockItem(blockToReturn, new FabricItemSettings()));
+    /**
+     * Registers a block with its corresponding item in the game registry.
+     *
+     * @param id  The id of the block
+     * @param block The block to register
+     * @return The registered block
+     */
+    public Block registerBlockWithItem(String id, Block block) {
+        Block blockToReturn = registerBlock(id, block);
+        registerItem(id, new BlockItem(blockToReturn, new FabricItemSettings()));
         return blockToReturn;
     }
 
-    public Item registerItem(String name, Item item) {
-        return Registry.register(Registries.ITEM, getIdentifier(name), item);
+    /**
+     * Registers an item with the game registry.
+     *
+     * @param id The id of the item
+     * @param item The item to register
+     * @return The registered item
+     */
+    public Item registerItem(String id, Item item) {
+        return Registry.register(Registries.ITEM, getIdentifier(id), item);
     }
 
-    public <T extends BlockEntity> BlockEntityType<T> registerBlockEntity(String name, BlockEntityType<T> blockEntity) {
-        return Registry.register(Registries.BLOCK_ENTITY_TYPE, getIdentifier(name), blockEntity);
+    /**
+     * Registers a block entity with the game registry.
+     *
+     * @param id        The id of the block entity
+     * @param blockEntity The block entity to register
+     * @return The registered block entity type
+     */
+    public <T extends BlockEntity> BlockEntityType<T> registerBlockEntity(String id, BlockEntityType<T> blockEntity) {
+        return Registry.register(Registries.BLOCK_ENTITY_TYPE, getIdentifier(id), blockEntity);
     }
 
-    public <T, R extends Registry<T>> TagKey<T> registerTag(RegistryKey<R> tagRegistry, String name) {
-        return TagKey.of(tagRegistry, getIdentifier(name));
+    /**
+     * Registers an item group with the game registry.
+     *
+     * @param id      The id of the item group
+     * @param itemGroup The item group to register
+     * @return The registered item group
+     */
+    public ItemGroup registerItemGroup(String id, ItemGroup itemGroup) {
+        return Registry.register(Registries.ITEM_GROUP, getIdentifier(id), itemGroup);
     }
 
-    public ItemGroup registerItemGroup(String name, ItemGroup itemGroup) {
-        return Registry.register(Registries.ITEM_GROUP, getIdentifier(name), itemGroup);
+    /**
+     * Registers an entity type with the game registry.
+     *
+     * @param id   The id of the entity type
+     * @param entity The entity type to register
+     * @return The registered entity type
+     */
+    public <T extends Entity> EntityType<T> registerEntity(String id, EntityType<T> entity) {
+        return Registry.register(Registries.ENTITY_TYPE, getIdentifier(id), entity);
     }
 
+    /**
+     * Registers an enchantment with the game registry.
+     *
+     * @param id       The id of the enchantment
+     * @param enchantment The enchantment to register
+     * @return The registered enchantment
+     */
+    public Enchantment registerEnchantment(String id, Enchantment enchantment) {
+        return Registry.register(Registries.ENCHANTMENT, getIdentifier(id), enchantment);
+    }
+
+    /**
+     * Registers a tag key with the game registry.
+     *
+     * @param <T>        The type of the tag
+     * @param <R>        The type of the registry
+     * @param tagRegistry The registry key for the tag
+     * @param id        The id of the tag
+     * @return The registered tag key
+     */
+    public <T, R extends Registry<T>> TagKey<T> registerTag(RegistryKey<R> tagRegistry, String id) {
+        return TagKey.of(tagRegistry, getIdentifier(id));
+    }
+
+    /**
+     * Registers a screen handler type with the game registry.
+     *
+     * @param <H>     The type of the screen handler
+     * @param <S>     The type of the handled screen
+     * @param <T>     The type of the screen handler type
+     * @param id    The id of the screen handler
+     * @param handler The screen handler type to register
+     * @param screen  The provider for the handled screen
+     * @return The registered screen handler type
+     */
     public <H extends ScreenHandler, S extends HandledScreen<H>, T extends ScreenHandlerType<H>>
-    ScreenHandlerType<H> registerScreen(String name, T handler, HandledScreens.Provider<H, S> screen) {
-        ScreenHandlerType<H> handlerTypeToReturn = Registry.register(Registries.SCREEN_HANDLER, getIdentifier(name), handler);
+    ScreenHandlerType<H> registerScreen(String id, T handler, HandledScreens.Provider<H, S> screen) {
+        ScreenHandlerType<H> handlerTypeToReturn = Registry.register(Registries.SCREEN_HANDLER, getIdentifier(id), handler);
         HandledScreens.register(handlerTypeToReturn, screen);
         return handlerTypeToReturn;
     }
 
+    /**
+     * Gets the mod id associated with this {@code RegistryHelper}.
+     *
+     * @return The mod id
+     */
     public String getModId() {
         return modId;
     }
 
+    /**
+     * Creates an {@code Identifier} using the mod id and the specified id.
+     *
+     * @param id The id for the {@code Identifier}
+     * @return The created {@code Identifier}
+     */
     public Identifier getIdentifier(String id) {
         return new Identifier(modId, id);
     }
 
 }
+
--- src/main/java/com/mikitellurium/telluriumforge/util/MouseUtils.java
+++ src/main/java/com/mikitellurium/telluriumforge/util/MouseUtils.java
@@ -2,12 +2,35 @@ package com.mikitellurium.telluriumforge.util;
 
 import net.minecraft.client.util.math.Rect2i;
 
+/**
+ * The {@code MouseUtils} class provides utility methods for handling mouse-related operations.
+ */
 public class MouseUtils {
 
+    /**
+     * Checks if the specified mouse coordinates are within the boundaries of a rectangular area.
+     *
+     * @param mouseX The X-coordinate of the mouse
+     * @param mouseY The Y-coordinate of the mouse
+     * @param xPos   The X-coordinate of the top-left corner of the area
+     * @param yPos   The Y-coordinate of the top-left corner of the area
+     * @param sizeX  The width of the area
+     * @param sizeY  The height of the area
+     * @return {@code true} if the mouse is above the specified area, otherwise {@code false}
+     */
     public static boolean isAboveArea(double mouseX, double mouseY, int xPos, int yPos, int sizeX, int sizeY) {
         return (mouseX >= xPos && mouseX <= xPos + sizeX) && (mouseY >= yPos && mouseY <= yPos + sizeY);
     }
 
+    /**
+     * Checks if the specified mouse coordinates are within the boundaries of a rectangular area.
+     * This method takes a {@code Rect2i} object representing the area.
+     *
+     * @param mouseX The X-coordinate of the mouse
+     * @param mouseY The Y-coordinate of the mouse
+     * @param area   The rectangular area defined by a {@code Rect2i} object
+     * @return {@code true} if the mouse is above the specified area, otherwise {@code false}
+     */
     public static boolean isAboveArea(double mouseX, double mouseY, Rect2i area) {
         return isAboveArea(mouseX, mouseY, area.getX(), area.getY(), area.getWidth(), area.getHeight());
     }
