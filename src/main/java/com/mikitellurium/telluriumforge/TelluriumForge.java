ded5f1019bbff8a571958f8b701cb43aa5e69eb1
Miki_Tellurium <michelegamer14@gmail.com>
2024-01-18 14:48:51 +0100

    Removed unnecessary code

--- src/main/java/com/mikitellurium/telluriumforge/TelluriumForge.java
+++ src/main/java/com/mikitellurium/telluriumforge/TelluriumForge.java
@@ -1,14 +1,6 @@
 package com.mikitellurium.telluriumforge;
 
-import com.mikitellurium.telluriumforge.test.ExampleConfig;
-import com.mikitellurium.telluriumforge.test.TestEvents;
 import net.fabricmc.api.ModInitializer;
-
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
-import net.fabricmc.fabric.api.event.EventFactory;
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents;
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
-import net.minecraft.world.WorldEvents;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,8 +11,6 @@ public class TelluriumForge implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		TestEvents.registerEvents();
-		ExampleConfig.buildConfig();
 	}
 
 	public static String modId() {
