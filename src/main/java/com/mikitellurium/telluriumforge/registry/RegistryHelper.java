6319a1be0a4b6cf3d5e7d93cff2df06448ed1236
Miki_Tellurium <michelegamer14@gmail.com>
2024-03-10 18:54:53 +0100

    Changed argument name id to path

--- src/main/java/com/mikitellurium/telluriumforge/registry/RegistryHelper.java
+++ src/main/java/com/mikitellurium/telluriumforge/registry/RegistryHelper.java
@@ -45,80 +45,80 @@ public class RegistryHelper implements SimpleRegistrationHelper {
     /**
      * Registers a block with the game registry.
      *
-     * @param id  The id of the block
+     * @param path  The path of the block
      * @param block The block to register
      * @return The registered block
      */
-    public Block registerBlock(String id, Block block) {
-        return Registry.register(Registries.BLOCK, modId(id), block);
+    public Block registerBlock(String path, Block block) {
+        return Registry.register(Registries.BLOCK, modId(path), block);
     }
 
     /**
      * Registers a block with its corresponding item in the game registry.
      *
-     * @param id  The id of the block
+     * @param path  The path of the block
      * @param block The block to register
      * @return The registered block
      */
-    public Block registerBlockWithItem(String id, Block block) {
-        Block blockToReturn = registerBlock(id, block);
-        registerItem(id, new BlockItem(blockToReturn, new FabricItemSettings()));
+    public Block registerBlockWithItem(String path, Block block) {
+        Block blockToReturn = registerBlock(path, block);
+        registerItem(path, new BlockItem(blockToReturn, new FabricItemSettings()));
         return blockToReturn;
     }
 
     /**
      * Registers an item with the game registry.
      *
-     * @param id The id of the item
+     * @param path The path of the item
      * @param item The item to register
      * @return The registered item
      */
-    public Item registerItem(String id, Item item) {
-        return Registry.register(Registries.ITEM, modId(id), item);
+    public Item registerItem(String path, Item item) {
+        return Registry.register(Registries.ITEM, modId(path), item);
     }
 
     /**
      * Registers a block entity with the game registry.
      *
-     * @param id        The id of the block entity
+     * @param path The path of the block entity
      * @param blockEntity The block entity to register
      * @return The registered block entity type
      */
-    public <T extends BlockEntity> BlockEntityType<T> registerBlockEntity(String id, BlockEntityType<T> blockEntity) {
-        return Registry.register(Registries.BLOCK_ENTITY_TYPE, modId(id), blockEntity);
+    public <T extends BlockEntity> BlockEntityType<T> registerBlockEntity(String path, BlockEntityType<T> blockEntity) {
+        return Registry.register(Registries.BLOCK_ENTITY_TYPE, modId(path), blockEntity);
     }
 
     /**
      * Registers an item group with the game registry.
      *
-     * @param id      The id of the item group
+     * @param path      The path of the item group
      * @param itemGroup The item group to register
      * @return The registered item group
      */
-    public ItemGroup registerItemGroup(String id, ItemGroup itemGroup) {
-        return Registry.register(Registries.ITEM_GROUP, modId(id), itemGroup);
+    public ItemGroup registerItemGroup(String path, ItemGroup itemGroup) {
+        return Registry.register(Registries.ITEM_GROUP, modId(path), itemGroup);
     }
 
     /**
      * Registers an entity type with the game registry.
      *
-     * @param id   The id of the entity type
+     * @param path   The path of the entity type
      * @param entity The entity type to register
      * @return The registered entity type
      */
-    public <T extends Entity> EntityType<T> registerEntity(String id, EntityType<T> entity) {
-        return Registry.register(Registries.ENTITY_TYPE, modId(id), entity);
+    public <T extends Entity> EntityType<T> registerEntity(String path, EntityType<T> entity) {
+        return Registry.register(Registries.ENTITY_TYPE, modId(path), entity);
     }
 
     /**
      * Registers an enchantment with the game registry.
      *
-     * @param id       The id of the enchantment
+     * @param path       The path of the enchantment
      * @param enchantment The enchantment to register
      * @return The registered enchantment
      */
-    public Enchantment registerEnchantment(String id, Enchantment enchantment) {
-        return Registry.register(Registries.ENCHANTMENT, modId(id), enchantment);
+    public Enchantment registerEnchantment(String path, Enchantment enchantment) {
+        return Registry.register(Registries.ENCHANTMENT, modId(path), enchantment);
     }
 
     /**
@@ -127,11 +127,11 @@ public class RegistryHelper implements SimpleRegistrationHelper {
      * @param <T>        The type of the tag
      * @param <R>        The type of the registry
      * @param tagRegistry The registry key for the tag
-     * @param id        The id of the tag
+     * @param path        The path of the tag
      * @return The registered tag key
      */
-    public <T, R extends Registry<T>> TagKey<T> registerTag(RegistryKey<R> tagRegistry, String id) {
-        return TagKey.of(tagRegistry, modId(id));
+    public <T, R extends Registry<T>> TagKey<T> registerTag(RegistryKey<R> tagRegistry, String path) {
+        return TagKey.of(tagRegistry, modId(path));
     }
 
     /**
@@ -140,14 +140,14 @@ public class RegistryHelper implements SimpleRegistrationHelper {
      * @param <H>     The type of the screen handler
      * @param <S>     The type of the handled screen
      * @param <T>     The type of the screen handler type
-     * @param id    The id of the screen handler
+     * @param path    The path of the screen handler
      * @param handler The screen handler type to register
      * @param screen  The provider for the handled screen
      * @return The registered screen handler type
      */
     public <H extends ScreenHandler, S extends HandledScreen<H>, T extends ScreenHandlerType<H>>
-    ScreenHandlerType<H> registerScreen(String id, T handler, HandledScreens.Provider<H, S> screen) {
-        ScreenHandlerType<H> handlerTypeToReturn = Registry.register(Registries.SCREEN_HANDLER, modId(id), handler);
+    ScreenHandlerType<H> registerScreen(String path, T handler, HandledScreens.Provider<H, S> screen) {
+        ScreenHandlerType<H> handlerTypeToReturn = Registry.register(Registries.SCREEN_HANDLER, modId(path), handler);
         HandledScreens.register(handlerTypeToReturn, screen);
         return handlerTypeToReturn;
     }
