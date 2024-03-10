97ff512d69b3e3dc74788c04fe31f362f492c841
Miki_Tellurium <michelegamer14@gmail.com>
2024-03-10 19:34:56 +0100

    Added a value type to the packet

--- src/main/java/com/mikitellurium/telluriumforge/networking/BlockEntitySyncPacket.java
+++ src/main/java/com/mikitellurium/telluriumforge/networking/BlockEntitySyncPacket.java
@@ -7,17 +7,22 @@ import net.minecraft.util.math.BlockPos;
 
 /**
  * A basic class used to send packets for block entities data synchronization.
+ * This packet only sends the {@code BlockPos} of the {@code BlockEntity} and a
+ * single value of type T.
  * <p>
  * Implementations should store the packet type instance in a static final
  * field and return that in the {@link #getType()} method implemented from
  * the {@code FabricPacket} interface.
+ *
+ * @param <T> The type of data that this packet send
  */
-public abstract class BlockEntitySyncPacket implements FabricPacket {
+public abstract class BlockEntitySyncPacket<T> implements FabricPacket {
 
     /**
      * The {@code BlockPos} of the block entity
      */
     private final BlockPos blockPos;
+    private final T value;
 
     /**
      * Construct a new packet with the {@code BlockPos} of the
@@ -25,8 +30,9 @@ public abstract class BlockEntitySyncPacket implements FabricPacket {
      *
      * @param blockPos the {@code BlockPos} of the block entity to sync
      */
-    public BlockEntitySyncPacket(BlockPos blockPos) {
+    public BlockEntitySyncPacket(BlockPos blockPos, T value) {
         this.blockPos = blockPos;
+        this.value = value;
     }
 
     /**
@@ -39,12 +45,14 @@ public abstract class BlockEntitySyncPacket implements FabricPacket {
         return blockPos;
     }
 
-    @Override
-    public void write(PacketByteBuf buf) {
-        buf.writeBlockPos(this.blockPos);
+    /**
+     * Gets the value used to sync to the block entity
+     * stored in this packet.
+     *
+     * @return the value stored in this packet
+     */
+    public T getValue() {
+        return value;
     }
-
-    @Override
-    public abstract PacketType<?> getType();
 
 }
