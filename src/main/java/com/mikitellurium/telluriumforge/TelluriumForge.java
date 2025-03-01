bfdce4ddbc1c122b41f40646d1231bdc6ab761aa
Miki_Tellurium <michelegamer14@gmail.com>
2025-03-01 12:04:33 +0100

    Removed test code

--- src/main/java/com/mikitellurium/telluriumforge/TelluriumForge.java
+++ src/main/java/com/mikitellurium/telluriumforge/TelluriumForge.java
@@ -1,11 +1,6 @@
 package com.mikitellurium.telluriumforge;
 
-import com.mikitellurium.telluriumforge.test.NetworkingTest;
-import com.mikitellurium.telluriumforge.test.RegistryTest;
-import com.mikitellurium.telluriumforge.test.TestEvents;
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,10 +11,6 @@ public class TelluriumForge {
     private static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	public TelluriumForge() {
-		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-		RegistryTest.register(modEventBus);
-		TestEvents.registerEvents(modEventBus);
-		NetworkingTest.register();
 	}
 
 	public static String modId() {
