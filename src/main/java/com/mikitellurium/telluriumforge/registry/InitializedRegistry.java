49c78de03c04abea638adbe2cec259321873e557
Miki_Tellurium <michelegamer14@gmail.com>
2025-03-01 22:36:26 +0100

    Changed registration classes

--- src/main/java/com/mikitellurium/telluriumforge/registry/InitializedRegistry.java
+++
@@ -1,9 +0,0 @@
-package com.mikitellurium.telluriumforge.registry;
-
-import net.minecraftforge.registries.DeferredRegister;
-
-public interface InitializedRegistry<T> {
-
-    void init(DeferredRegister<T> deferredRegister);
-
-}
