a7eab921a9c572906d6be56d4f903a4ee29e9800
Miki_Tellurium <michelegamer14@gmail.com>
2025-03-05 20:32:05 +0100

    Added uv coordinates to sprite

--- src/main/java/com/mikitellurium/telluriumforge/util/SimpleSprite.java
+++
@@ -1,6 +0,0 @@
-package com.mikitellurium.telluriumforge.util;
-
-import net.minecraft.resources.ResourceLocation;
-
-public record SimpleSprite(ResourceLocation texture, int width, int height) {
-}
---
+++ src/main/java/com/mikitellurium/telluriumforge/util/TextureSprite.java
@@ -0,0 +1,6 @@
+package com.mikitellurium.telluriumforge.util;
+
+import net.minecraft.resources.ResourceLocation;
+
+public record TextureSprite(ResourceLocation texture, int uOffset, int vOffset, int width, int height) {
+}
