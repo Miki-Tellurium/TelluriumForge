a2e8138a300a03da3add88da91e4b75599b0a059
Miki_Tellurium <michelegamer14@gmail.com>
2025-03-05 20:52:05 +0100

    Added x y coordinates to sprite

--- src/main/java/com/mikitellurium/telluriumforge/util/TextureSprite.java
+++ src/main/java/com/mikitellurium/telluriumforge/util/TextureSprite.java
@@ -2,5 +2,5 @@ package com.mikitellurium.telluriumforge.util;
 
 import net.minecraft.resources.ResourceLocation;
 
-public record TextureSprite(ResourceLocation texture, int uOffset, int vOffset, int width, int height) {
+public record TextureSprite(ResourceLocation texture, int uOffset, int vOffset, int width, int height, int xPos, int yPos) {
 }
