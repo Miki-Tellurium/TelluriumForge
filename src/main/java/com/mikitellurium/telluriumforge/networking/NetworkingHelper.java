841d29b8850bc90a90f9cdcc6f9db027127d056b
Miki_Tellurium <michelegamer14@gmail.com>
2025-04-24 15:21:14 +0200

    Made networking helper instantiable

--- src/main/java/com/mikitellurium/telluriumforge/networking/NetworkingHelper.java
+++ src/main/java/com/mikitellurium/telluriumforge/networking/NetworkingHelper.java
@@ -8,12 +8,15 @@ import net.minecraftforge.network.NetworkRegistry;
 import net.minecraftforge.network.PacketDistributor;
 import net.minecraftforge.network.simple.SimpleChannel;
 
+import java.util.function.Consumer;
 import java.util.function.Function;
 
 public class NetworkingHelper {
 
-    public static SimpleChannel simpleChannel(String modId) {
-        return NetworkRegistry.ChannelBuilder
+    private final SimpleChannel channel;
+
+    private NetworkingHelper(String modId) {
+        this.channel = NetworkRegistry.ChannelBuilder
                 .named(new ResourceLocation(modId, "messages"))
                 .networkProtocolVersion(() -> "1.0")
                 .clientAcceptedVersions(s -> true)
@@ -21,12 +24,36 @@ public class NetworkingHelper {
                 .simpleChannel();
     }
 
-    public static <T extends ModPacket> void registerPacket(SimpleChannel channel, int id, Class<T> clazz, Function<FriendlyByteBuf, T> factory, NetworkDirection networkDirection) {
+    public static NetworkingHelper create(String modId) {
+        return new NetworkingHelper(modId);
+    }
+
+    public <P extends ModPacket> void registerPacket(int id, Class<P> clazz, Function<FriendlyByteBuf, P> factory, NetworkDirection networkDirection) {
         channel.messageBuilder(clazz, id, networkDirection)
                 .decoder(factory)
-                .encoder(T::write)
-                .consumerMainThread(T::handle)
+                .encoder(P::write)
+                .consumerMainThread(P::handle)
                 .add();
+    }
+
+    public void simpleChannel(Consumer<SimpleChannel> consumer) {
+        consumer.accept(channel);
+    }
+
+    public <P extends ModPacket> void send(PacketDistributor.PacketTarget target, P message) {
+        channel.send(target, message);
+    }
+
+    public <P extends ModPacket> void sendToServer(P message) {
+        channel.sendToServer(message);
+    }
+
+    public <P extends ModPacket> void sendToClientPlayer(P message, ServerPlayer player) {
+        this.send(PacketDistributor.PLAYER.with(() -> player), message);
+    }
+
+    public <P extends ModPacket> void sendToClients(P message) {
+        this.send(PacketDistributor.ALL.noArg(), message);
     }
 
 }
