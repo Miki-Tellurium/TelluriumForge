7a95f34739c0fe55e1026c9e162055534382414b
Miki_Tellurium <michelegamer14@gmail.com>
2025-05-08 19:51:39 +0200

    Renamed class and methods

---
+++ src/main/java/com/mikitellurium/telluriumforge/registry/ResourceLocationProvider.java
@@ -0,0 +1,26 @@
+package com.mikitellurium.telluriumforge.registry;
+
+import net.minecraft.resources.ResourceLocation;
+
+public interface ResourceLocationProvider {
+
+    /**
+     * @return The mod id
+     */
+    String modId();
+
+    /**
+     * @return A {@link ResourceLocation} with this provider mod id.
+     */
+    default ResourceLocation ofMod(String path) {
+        return ResourceLocation.fromNamespaceAndPath(this.modId(), path);
+    }
+
+    /**
+     * @return A {@link ResourceLocation} with the default namespace.
+     */
+    default ResourceLocation ofMc(String path) {
+        return ResourceLocation.withDefaultNamespace(path);
+    }
+
+}
