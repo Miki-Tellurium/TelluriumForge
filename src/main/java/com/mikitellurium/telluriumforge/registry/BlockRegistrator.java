b67205f1822b24adbd1910c6f8fe243e157854f3
miki <michelegamer14@gmail.com>
2025-10-19 18:59:47 +0200

    Added model loader location

--- src/main/java/com/mikitellurium/telluriumforge/registry/BlockRegistrator.java
+++ src/main/java/com/mikitellurium/telluriumforge/registry/BlockRegistrator.java
@@ -1,9 +1,10 @@
 package com.mikitellurium.telluriumforge.registry;
 
 import net.minecraft.block.Block;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
-import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.common.registry.ForgeRegistries;
 import net.minecraftforge.registries.IForgeRegistry;
 
@@ -30,18 +31,15 @@ public class BlockRegistrator extends Registrator<Block> {
 
     public <S extends Block> S registerWithItem(String id, S block) {
         S blockObj = this.register(id, block);
-        this.registerBlockItem(id);
+        this.registerBlockItem(id, block);
         return blockObj;
     }
 
     @SuppressWarnings("ConstantConditions")
-    public ItemBlock registerBlockItem(String id) {
-        ResourceLocation loc = modLoc(id);
-        if (!ForgeRegistries.BLOCKS.containsKey(loc)) {
-            throw new RuntimeException("Block " + loc + " not registered yet.");
-        }
-        ItemBlock item = (ItemBlock) new ItemBlock(ForgeRegistries.BLOCKS.getValue(loc)).setRegistryName(modLoc(id)).setTranslationKey(id);
+    public ItemBlock registerBlockItem(String id, Block block) {
+        ItemBlock item = (ItemBlock) new ItemBlock(block).setRegistryName(modLoc(id)).setTranslationKey(id);
         itemRegistry.register(item);
+        ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation(modLoc(id), "inventory"));
         return item;
     }
 }
