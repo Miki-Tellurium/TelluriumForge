f5f42468ef73f1974f7a564caedbe12068f0d374
miki <michelegamer14@gmail.com>
2025-10-19 12:01:41 +0200

    Set translation keys

--- src/main/java/com/mikitellurium/telluriumforge/registry/BlockRegistrator.java
+++ src/main/java/com/mikitellurium/telluriumforge/registry/BlockRegistrator.java
@@ -3,6 +3,7 @@ package com.mikitellurium.telluriumforge.registry;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.registry.ForgeRegistries;
 import net.minecraftforge.registries.IForgeRegistry;
 
@@ -21,14 +22,25 @@ public class BlockRegistrator extends Registrator<Block> {
         this.itemRegistry = ForgeRegistries.ITEMS;
     }
 
+    @SuppressWarnings("unchecked")
+    @Override
+    public <S extends Block> S register(String id, S object) {
+        return super.register(id, (S) object.setTranslationKey(id));
+    }
+
     public <S extends Block> S registerWithItem(String id, S block) {
         S blockObj = this.register(id, block);
-        registerBlockItem(id, blockObj);
+        this.registerBlockItem(id);
         return blockObj;
     }
 
-    public <S extends Block> ItemBlock registerBlockItem(String id, S block) {
-        ItemBlock item = (ItemBlock) new ItemBlock(block).setRegistryName(modLoc(id));
+    @SuppressWarnings("ConstantConditions")
+    public ItemBlock registerBlockItem(String id) {
+        ResourceLocation loc = modLoc(id);
+        if (!ForgeRegistries.BLOCKS.containsKey(loc)) {
+            throw new RuntimeException("Block " + loc + " not registered yet.");
+        }
+        ItemBlock item = (ItemBlock) new ItemBlock(ForgeRegistries.BLOCKS.getValue(loc)).setRegistryName(modLoc(id)).setTranslationKey(id);
         itemRegistry.register(item);
         return item;
     }
