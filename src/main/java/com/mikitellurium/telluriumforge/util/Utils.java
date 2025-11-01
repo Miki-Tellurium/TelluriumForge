a062c52170476724b1f21e13b77af8c18d0a0f4f
miki <michelegamer14@gmail.com>
2025-11-01 12:38:33 +0100

    Added get random form list

--- src/main/java/com/mikitellurium/telluriumforge/util/Utils.java
+++ src/main/java/com/mikitellurium/telluriumforge/util/Utils.java
@@ -1,10 +1,29 @@
 package com.mikitellurium.telluriumforge.util;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.Random;
 import java.util.function.Consumer;
 
 public class Utils {
     public static <T> T make(T obj, Consumer<T> consumer) {
         consumer.accept(obj);
         return obj;
+    }
+
+    public static <T> T getRandom(T[] objects, Random random) {
+        return objects[random.nextInt(objects.length)];
+    }
+
+    public static int getRandom(int[] objects, Random random) {
+        return objects[random.nextInt(objects.length)];
+    }
+
+    public static <T> T getRandom(List<T> objects, Random random) {
+        return objects.get(random.nextInt(objects.size()));
+    }
+
+    public static <T> Optional<T> getRandomSafe(List<T> objects, Random random) {
+        return objects.isEmpty() ? Optional.empty() : Optional.of(getRandom(objects, random));
     }
 }
